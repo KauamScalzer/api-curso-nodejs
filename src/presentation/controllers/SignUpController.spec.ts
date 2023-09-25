681675a44d3934a679198464d67a70ad22fa6b58
Kauam Scalzer <scalzerkauam@gmail.com>
2023-09-24 23:15:08 -0300

    refactor: remove code duplicated

--- src/presentation/controllers/SignUpController.spec.ts
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -98,7 +98,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
 
-  test('Should return 400 password confirmation fails', () => {
+  test('Should return 400 if password confirmation fails', () => {
     const { sut, emailValidatorStub } = makeSut()
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
     const httpRequest = {
