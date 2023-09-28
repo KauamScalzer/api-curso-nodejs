5ec6147f6e381d254774625d9d8b5d52801f2d3a
Kauam Scalzer <scalzerkauam@gmail.com>
2023-09-28 05:15:27 -0300

    refactor: create folder to a file

--- src/data/usecases/account/DbCreateAccount.spec.ts
+++ src/data/usecases/account/DbCreateAccount.spec.ts
@@ -1,4 +1,4 @@
-import { Encrypter } from 'data/protocols/encrypter'
+import { Encrypter } from 'data/protocols/encrypter/encrypter'
 import { DbCreateAccount } from './DbCreateAccount'
 import { CreateAccountModel } from 'domain/usecases/account'
 import { AccountModel } from 'domain/models'
