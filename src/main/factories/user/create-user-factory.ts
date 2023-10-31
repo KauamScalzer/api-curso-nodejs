86e7210aa4d83f7b269661efec3d5495fb6986a0
Kauam Scalzer <kauam.souza@matrixdobrasil.com.br>
2023-10-31 20:36:20 -0300

    feat: ensure logControllerDecorator calls controller with correct value

--- src/main/factories/user/create-user-factory.ts
+++ src/main/factories/user/create-user-factory.ts
@@ -3,13 +3,15 @@ import { EmailValidatorAdapter } from '../../../utils'
 import { CreateUserUsecase } from '../../../data/usecases/user'
 import { BcryptAdapter } from '../../../infra/criptography'
 import { CreateUser } from '../../../infra/db/repositories/user'
+import { Controller } from '../../../presentation/protocols'
+import { LogControllerDecorator } from '../../decorators'
 
-export const makeCreateUserController = (): CreateUserController => {
+export const makeCreateUserController = (): Controller => {
   const salt = 12
   const createUser = new CreateUser()
   const bcryptAdapter = new BcryptAdapter(salt)
   const createUserUsecase = new CreateUserUsecase(bcryptAdapter, createUser)
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const createUserController = new CreateUserController(emailValidatorAdapter, createUserUsecase)
-  return createUserController
+  return new LogControllerDecorator(createUserController)
 }
