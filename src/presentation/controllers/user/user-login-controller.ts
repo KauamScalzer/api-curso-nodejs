87398cc973a47a4eb486ac2a3441543a02e77c37
Kauam Scalzer <kauam.souza@matrixdobrasil.com.br>
2023-11-02 15:17:14 -0300

    feat: ensure UserLoginController return 500 if EmailValidator throws

--- src/presentation/controllers/user/user-login-controller.ts
+++ src/presentation/controllers/user/user-login-controller.ts
@@ -1,5 +1,5 @@
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
-import { badRequest, ok } from '../../helpers'
+import { badRequest, ok, serverError } from '../../helpers'
 import { InvalidParamError, MissingParamError } from '../../errors'
 import { EmailValidator } from '../../../presentation/protocols'
 
@@ -9,17 +9,21 @@ export class UserLoginController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body
-    if (!email) {
-      return badRequest(new MissingParamError('email'))
+    try {
+      const { email, password } = httpRequest.body
+      if (!email) {
+        return badRequest(new MissingParamError('email'))
+      }
+      if (!password) {
+        return badRequest(new MissingParamError('password'))
+      }
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+      return ok('')
+    } catch (error: any) {
+      return serverError(error)
     }
-    if (!password) {
-      return badRequest(new MissingParamError('password'))
-    }
-    const isValid = this.emailValidator.isValid(email)
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
-    }
-    return ok('')
   }
 }
