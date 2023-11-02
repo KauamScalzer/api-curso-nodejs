16e6fb037031ea1c129b7d98dd9c8331342e7393
Kauam Scalzer <kauam.souza@matrixdobrasil.com.br>
2023-11-02 15:49:01 -0300

    feat: ensure UserLoginController call UserAuthentication with correct values

--- src/domain/usecases/user/index.ts
+++ src/domain/usecases/user/index.ts
@@ -1,1 +1,2 @@
 export { ICreateUserUsecase, CreateUserModel } from './create-user-usecase'
+export { UserAuthentication } from './user-authentication-usecase'
--- src/presentation/controllers/user/user-login-controller.ts
+++ src/presentation/controllers/user/user-login-controller.ts
@@ -2,10 +2,12 @@ import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { badRequest, ok, serverError } from '../../helpers'
 import { InvalidParamError, MissingParamError } from '../../errors'
 import { EmailValidator } from '../../../presentation/protocols'
+import { UserAuthentication } from '../../../domain/usecases/user'
 
 export class UserLoginController implements Controller {
   constructor (
-    private readonly emailValidator: EmailValidator
+    private readonly emailValidator: EmailValidator,
+    private readonly userAuthentication: UserAuthentication
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -21,6 +23,7 @@ export class UserLoginController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
+      await this.userAuthentication.auth(email, password)
       return ok('')
     } catch (error: any) {
       return serverError(error)
