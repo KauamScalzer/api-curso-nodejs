8c3cd01415b82784bf4fc02d6d42f6c73cbdc747
Kauam Scalzer <kauam.souza@matrixdobrasil.com.br>
2023-11-02 14:57:27 -0300

    feat: ensure UserLoginController return 400 if no password is provided

--- src/presentation/controllers/user/user-login-controller.ts
+++ src/presentation/controllers/user/user-login-controller.ts
@@ -1,9 +1,15 @@
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
-import { badRequest } from '../../helpers'
+import { badRequest, ok } from '../../helpers'
 import { MissingParamError } from '../../errors'
 
 export class UserLoginController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return badRequest(new MissingParamError('email'))
+    if (!httpRequest.body.email) {
+      return badRequest(new MissingParamError('email'))
+    }
+    if (!httpRequest.body.password) {
+      return badRequest(new MissingParamError('password'))
+    }
+    return ok('')
   }
 }
