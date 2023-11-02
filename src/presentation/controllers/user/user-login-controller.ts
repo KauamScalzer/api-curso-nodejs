f108f57529e72941af9c77baf925a30c77299c69
Kauam Scalzer <kauam.souza@matrixdobrasil.com.br>
2023-11-02 15:07:01 -0300

    feat: ensure UserLoginController call EmailValidator with correct email

--- src/presentation/controllers/user/user-login-controller.ts
+++ src/presentation/controllers/user/user-login-controller.ts
@@ -1,8 +1,13 @@
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { badRequest, ok } from '../../helpers'
 import { MissingParamError } from '../../errors'
+import { EmailValidator } from '../../../presentation/protocols'
 
 export class UserLoginController implements Controller {
+  constructor (
+    private readonly emailValidator: EmailValidator
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body.email) {
       return badRequest(new MissingParamError('email'))
@@ -10,6 +15,7 @@ export class UserLoginController implements Controller {
     if (!httpRequest.body.password) {
       return badRequest(new MissingParamError('password'))
     }
+    this.emailValidator.isValid(httpRequest.body.email)
     return ok('')
   }
 }
