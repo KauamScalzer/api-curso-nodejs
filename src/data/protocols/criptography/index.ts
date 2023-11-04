7a82f77383266f7f65b9edf60c3fa0c8bf8df5c7
Kauam Scalzer <scalzerkauam@gmail.com>
2023-11-03 22:02:43 -0300

    feat: ensure UserAuthenticationUsecase call TokenGenerator with correct id

--- src/data/protocols/criptography/index.ts
+++ src/data/protocols/criptography/index.ts
@@ -1,2 +1,3 @@
 export { Encrypter } from './encrypter'
 export { HashComparer } from './hash-comparer'
+export { TokenGenerator } from './token-generator'
--- src/data/usecases/user/user-authentication-usecase.ts
+++ src/data/usecases/user/user-authentication-usecase.ts
@@ -1,17 +1,19 @@
-import { HashComparer } from 'data/protocols/criptography'
+import { HashComparer, TokenGenerator } from 'data/protocols/criptography'
 import { GetOneUserByEmailRepository } from 'data/protocols/user'
 import { IUserAuthenticationUsecase, UserAuthenticationModel } from 'domain/usecases/user'
 
 export class UserAuthenticationUsecase implements IUserAuthenticationUsecase {
   constructor (
     private readonly getOneUserByEmailRepository: GetOneUserByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly tokenGenerator: TokenGenerator
   ) {}
 
   async auth (data: UserAuthenticationModel): Promise<string | null> {
     const user = await this.getOneUserByEmailRepository.getOne(data.email)
     if (user) {
       await this.hashComparer.compare(data.password, user.password)
+      await this.tokenGenerator.generate(user.id)
     }
     return null
   }
